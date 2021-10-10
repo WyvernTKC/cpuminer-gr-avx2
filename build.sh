2f0a4cd172fa889593454120cba4ea9dedcf161e
Matt Snoby <matt.snoby@icloud.com>
2021-10-10 09:30:57 -0400

    changed the default build

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ rm -f config.status
 ./autogen.sh || echo done
 
 # For GCC-9 && GCC-8
-#CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
+CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
 
 CFLAGS="-O3 -march=native -mtune=native" \
-CXXFLAGS="$CFLAGS -std=c++20 -Wno-ignored-attributes" \
+#CXXFLAGS="$CFLAGS -std=c++20 -Wno-ignored-attributes" \
 ./configure --with-curl
 
 make -j $(nproc)
