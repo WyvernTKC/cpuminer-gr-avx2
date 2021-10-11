55e681813647057f8327ba18ab472bd2b354466a
Michal Zurkowski <michal.zurkowski@gmail.com>
2021-10-11 18:35:46 +0000

    Make build.sh more friendly.
    Closes #33

--- build.sh
+++ build.sh
@@ -5,12 +5,28 @@ make distclean || echo clean
 rm -f config.status
 ./autogen.sh || echo done
 
-# For GCC-9 && GCC-8
-#CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
+GCC_VERSION=$(gcc --version | grep ^gcc | sed 's/^.* //g')
+GCC_MAJOR=$(echo $GCC_VERSION | cut -d. -f1)
 
-CFLAGS="-O3 -march=native -mtune=native" \
-CXXFLAGS="$CFLAGS -std=c++20 -Wno-ignored-attributes" \
-./configure --with-curl
+echo "Detected GCC ${GCC_VERSION} with Major ${GCC_MAJOR}"
+
+if [[ $GCC_MAJOR == 8 || $GCC_MAJOR == 9 ]]; then
+
+  CFLAGS="-O3 -march=native -mtune=native" \
+  CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
+  ./configure --with-curl
+
+elif [[ $GCC_MAJOR -ge 10 ]]; then
+
+  CFLAGS="-O3 -march=native -mtune=native" \
+  CXXFLAGS="$CFLAGS -std=c++20 -Wno-ignored-attributes" \
+  ./configure --with-curl
+
+else
+  echo "GCC version >= 8 is required for compilation"
+  exit
+fi
+
 
 make -j $(nproc)
 
