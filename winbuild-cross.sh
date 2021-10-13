dc98dbb6145c30c3f5baa6319b8b385212b5771b
Jimmy06 <Jimmy062006@users.noreply.github.com>
2021-10-13 21:15:45 +0100

    Update winbuild-cross.sh

--- winbuild-cross.sh
+++ winbuild-cross.sh
@@ -101,10 +101,13 @@ compile "znver1" "zen" "-mtune=znver1"
 compile "znver2" "zen2" "-mtune=znver2"
 
 # AMD Zen3 AVX2 SHA VAES
+if [ GCCVERSION -le 10 ]; then
 # GCC 10
 compile "znver3" "zen3" "-mtune=znver3"
 # GCC 9
-# compile "znver2" "zen3" "-mvaes -mtune=znver2"
+else
+compile "znver2" "zen3" "-mvaes -mtune=znver2"
+fi
 
 # Icelake AVX512 SHA VAES
 compile "icelake-client" "avx512-sha-vaes" "-mtune=intel"
